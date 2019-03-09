fe6aa7fecd20b0dd3533550210f7b5f40bf96eec
curtin <curtinliu@163.com>
2019-03-09 13:06:03 +0800

    修改路径名

--- src/main/java/com/bluewhale/spring/boot/BiueWhaleApplication.java
+++ src/main/java/com/bluewhale/spring/boot/BiueWhaleApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.bluewhale.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
