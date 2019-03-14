fee6bef77a84accd91efe092e813b5db41351ebb
curtin <curtinliu@163.com>
2019-03-14 22:18:08 +0800

    引入Bootstrap <add by curtin>

--- src/main/java/com/bluewhale/spring/boot/controller/BlueWhaleCtrl.java
+++ src/main/java/com/bluewhale/spring/boot/controller/BlueWhaleCtrl.java
@@ -2,6 +2,7 @@ package com.bluewhale.spring.boot.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @author curtin
  * @Date 2019-03-10 12:09:44
  */
-@RestController
+//@RestController
+@Controller
 public class BlueWhaleCtrl {
 	private static final Logger log=LoggerFactory.getLogger(BlueWhaleCtrl.class);
 	
 	@RequestMapping(value = "/hello")
-	public String index() {
+	public String hello() {
 		log.info("访问了controller");
 		return "Hello BlueWhale";
+	}
+	
+	@RequestMapping(value = "/index")
+	public String index() {
+		log.info("访问了index");
+		return "/index";
 	}
 	
 }
