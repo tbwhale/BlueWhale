42ab1027aeb34c33c139ee31f7241cff42587fed
ZhangXr <Administrator@MS-20180412MREE>
2019-03-21 11:33:32 +0800

    首页设计

---
+++ src/main/java/com/bluewhale/spring/boot/controller/DefaultViewController.java
@@ -0,0 +1,23 @@
+package com.bluewhale.spring.boot.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 锁定首页
+ * @author  张晓睿
+ * @version v0.1.0
+ * @Date    创建时间   2019年3月19日 下午8:32:28
+ */
+@Configuration
+public class DefaultViewController implements WebMvcConfigurer{
+
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("index");
+		WebMvcConfigurer.super.addViewControllers(registry);
+	}
+
+}
