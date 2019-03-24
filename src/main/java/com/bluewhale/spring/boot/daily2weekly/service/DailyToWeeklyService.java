91211cdae04602763bbccb6a82376b562a2e8134
Zhangxr <Zhangxr@192.168.2.103>
2019-03-24 22:56:53 +0800

    添加文件上传的js和css

---
+++ src/main/java/com/bluewhale/spring/boot/daily2weekly/service/DailyToWeeklyService.java
@@ -0,0 +1,10 @@
+package com.bluewhale.spring.boot.daily2weekly.service;
+
+/**
+ * @author ZhangXr
+ * @date 2019年3月24日
+ * @time 上午11:36:32
+ */
+public interface DailyToWeeklyService {
+
+}
---
+++ src/main/java/com/bluewhale/spring/boot/daily2weekly/service/impl/DailyToWeeklyServiceImpl.java
@@ -0,0 +1,12 @@
+package com.bluewhale.spring.boot.daily2weekly.service.impl;
+
+import com.bluewhale.spring.boot.daily2weekly.service.DailyToWeeklyService;
+
+/**
+ * @author ZhangXr
+ * @date 2019年3月24日
+ * @time 上午11:37:38
+ */
+public class DailyToWeeklyServiceImpl implements DailyToWeeklyService {
+
+}
